c3bd58bd929a0d2c596c9ac817cc596582c6c457
Maya <twebgrl56@gmail.com>
2026-06-15 17:30:50 -0400

    Add JavaScript scroll listener

---
+++ script.js
@@ -0,0 +1,11 @@
+const header = document.querySelector('header');
+
+if (header) {
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) {
+            header.classList.add('sticky');
+        } else {
+            header.classList.remove('sticky');
+        }
+    });
+}
